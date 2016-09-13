a122d2ab650e09145598b41d0ce3faae9a342d48
April Peng and HK Kahng <hk@>
2016-09-13 13:01:59 -0700

    create initial files and directories

---
+++ app/app.php
@@ -0,0 +1,15 @@
+<?php
+    date_default_timezone_set('America/Los_Angeles');
+    require_once __DIR__."/../vendor/autoload.php";
+    require_once __DIR__."/../src/xxx.php";
+
+    $app = new Silex\Application();
+    $app['debug'] = true;
+    $app->register(new Silex\Provider\TwigServiceProvider(), array('twig.path' => __DIR__.'/../views'));
+
+    $app->get('/', function() use ($app) {
+        return $app['twig']->render('xxx.html.twig');
+    });
+
+    return $app;
+?>
